246936b650e3046fc54b039f87c8f36508717515
DanielBelovol <danielbelovol08@gmail.com>
2024-01-20 13:45:35 +0200

    .

--- src/main/java/org/example/MigrationMain.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.flywaydb.core.Flyway;
-
-public class MigrationMain {
-    public static void main(String[] args) {
-        Flyway flyway = Flyway.configure().dataSource("jdbc:h2:file:./target/foobar", "sa", null).load();
-    }
-}
